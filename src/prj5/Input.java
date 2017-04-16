ced2be10d50b8ef436383ce4034e5d86c31fa626
GiveEmMoZo <zorianthornton5@gmail.com>
2017-04-16 16:26:08 -0400

    classes

--- src/prj5/Input.java
+++ src/prj5/Input.java
@@ -1,5 +1,12 @@
 package prj5;
 
+/**
+ * The main class for this project that will
+ * read the input files.
+ * @author Zorian Thornton  zorian15
+ * @version 2017.04.16
+ *
+ */
 public class Input
 {
 
