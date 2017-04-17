b9cd7192a51564c7b3335b537e5fc254591f0f05
GiveEmMoZo <zorianthornton5@gmail.com>
2017-04-17 17:20:36 -0400

    Person class

---
+++ src/prj5/Person.java
@@ -0,0 +1,14 @@
+package prj5;
+
+import java.util.Comparator;
+
+public class Person implements Comparator
+{
+
+    public int compare(Object arg0, Object arg1)
+    {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
