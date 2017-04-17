99465d09172032bda83813fcf0ebc173e0e1d0d4
GiveEmMoZo <zorianthornton5@gmail.com>
2017-04-17 18:42:33 -0400

    LinkedList

---
+++ src/prj5/LinkedList.java
@@ -0,0 +1,14 @@
+package prj5;
+
+/**
+ * The LinkedList class that will
+ * be used for data storage.
+ * @author Zorian Thornton  zorian15
+ * @version 2017.04.17
+ *
+ * @param <T>
+ */
+public class LinkedList<T>
+{
+
+}
