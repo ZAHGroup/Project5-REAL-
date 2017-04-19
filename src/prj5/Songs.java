db4c5fcea79b12d984564216f6727343439f048e
aayub98 <aayub98@vt.edu>
2017-04-19 17:42:04 -0400

    Added the methods we needed

--- src/prj5/Songs.java
+++ src/prj5/Songs.java
@@ -11,6 +11,40 @@ import java.util.Comparator;
  */
 public class Songs implements Comparator
 {
+    /**
+     * field for array for the hobbys that have heard a song the hobbys starting
+     * at [0] are Read, Art, Sports, and Music respectively
+     */
+    private int[] hobbyHeard = { 0, 0, 0, 0 };
+    /**
+     * field for array for the majors that have heard a song the majors starting
+     * at [0] are Math or CMDA, Computer Science, Other Engineering, and Other
+     * respectively
+     */
+    private int[] majorHeard = { 0, 0, 0, 0 };
+    /**
+     * field for array for the regions that have heard a song the regions in the
+     * array starting at [0] are Northeast US, Southeast US, the rest of US,
+     * Outside of United States
+     */
+    private int[] regionHeard = { 0, 0, 0, 0 };
+    /**
+     * field for array for the hobbys that have liked a song the hobbys starting
+     * at [0] are Read, Art, Sports, and Music respectively
+     */
+    private int[] hobbyLikes = { 0, 0, 0, 0 };
+    /**
+     * field for array for the majors that have heard a song the majors starting
+     * at [0] are Math or CMDA, Computer Science, Other Engineering, and Other
+     * respectively
+     */
+    private int[] majorLikes = { 0, 0, 0, 0 };
+    /**
+     * field for array for the regions that have heard a song the regions in the
+     * array starting at [0] are Northeast US, Southeast US, the rest of US,
+     * Outside of United States
+     */
+    private int[] regionLikes = { 0, 0, 0, 0 };
     // Song title
     private String title;
     
@@ -78,6 +112,132 @@ public class Songs implements Comparator
     {
         // TODO Auto-generated method stub
         return 0;
+    }
+    
+    /**
+     * returns the number of that heard for the hobby
+     * 
+     * @param heard
+     *            -the hobby you want
+     * @return hobbyHeard for the specified hobby
+     */
+    public int getHobbyHeard(int heard) {
+        return hobbyHeard[heard];
+    }
+
+    /**
+     * returns the number of that heard for the major
+     * 
+     * @param heard
+     *            -the major you want
+     * @return majorHeard for the specified major
+     */
+    public int getMajorHeard(int heard) {
+        return majorHeard[heard];
+    }
+
+    /**
+     * returns the number of people that heard for the region
+     * 
+     * @param heard
+     *            -the region you want
+     * @return regionHeard for the specified region
+     */
+    public int getRegionHeard(int heard) {
+        return regionHeard[heard];
+    }
+
+    /**
+     * returns the number of likes for the hobby
+     * 
+     * @param like
+     *            -the hobby you want
+     * @return hobbyLikes for the specified hobby
+     */
+    public int getHobbyLiked(int like) {
+        return hobbyLikes[like];
+    }
+
+    /**
+     * returns the number of likes for the major
+     * 
+     * @param like
+     *            -the major you want
+     * @return majorLikes for the specified major
+     */
+    public int getMajorLiked(int like) {
+        return majorLikes[like];
+    }
+
+    /**
+     * returns the number of likes for the region
+     * 
+     * @param like
+     *            -the region you want
+     * @return regionLikes for the specified region
+     */
+    public int getRegionLiked(int like) {
+        return regionLikes[like];
+    }
+
+    /**
+     * increments the array
+     * 
+     * @param heard
+     *            adds a heard to the hobbys heard count
+     */
+    public void addHeardHobby(int heard) {
+        hobbyHeard[heard] += 1;
+    }
+
+    /**
+     * increments the array
+     * 
+     * @param heard
+     *            adds a heard to the majors heard counnt
+     */
+    public void addHeardMajor(int heard) {
+        majorHeard[heard] += 1;
+    }
+
+    /**
+     * increments the array
+     * 
+     * @param heard
+     *            adds a heard to the regions heard count
+     */
+    public void addHeardRegion(int heard) {
+        regionHeard[heard] += 1;
+    }
+
+    /**
+     * increments the array
+     * 
+     * @param like
+     *            adds a like to the hobbys likes
+     */
+    public void addLikedHobby(int like) {
+        hobbyLikes[like] += 1;
+    }
+
+    /**
+     * increments the array
+     * 
+     * @param like
+     *            adds a like to the majors likes
+     */
+    public void addLikedMajor(int like) {
+        majorLikes[like] += 1;
+    }
+
+    /**
+     * increments the array
+     * 
+     * @param like
+     *            adds a like to the regions likes
+     */
+    public void addLikedRegion(int like) {
+        regionLikes[like] += 1;
     }
 
 }
